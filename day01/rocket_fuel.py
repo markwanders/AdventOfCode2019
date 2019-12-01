a04cfccf34730c6717889842a6fb2566f4318178
Mark Wanders <mndmwanders@gmail.com>
2019-12-01 12:50:09 +0100

    Improved day 1

--- day01/rocket_fuel.py
+++ day01/rocket_fuel.py
@@ -5,22 +5,22 @@ def fuel_for_mass(m):
     return math.floor(m / 3) - 2
 
 
+def iterative_fuel_for_mass(m):
+    module_fuel = additional_mass = fuel_for_mass(m)
+    while fuel_for_mass(additional_mass) > 0:
+        additional_mass = fuel_for_mass(additional_mass)
+        module_fuel += additional_mass
+    return module_fuel
+
+
 with open("input.txt") as my_file:
     modules = my_file.readlines()
 modules = [int(i) for i in modules]
 
 # part 1
-fuel = 0
-for mass in modules:
-    fuel += fuel_for_mass(mass)
+fuel = sum([fuel_for_mass(mass) for mass in modules])
 print("Total fuel needed is: " + str(fuel))
 
 # part 2
-total_fuel = 0
-for mass in modules:
-    module_fuel = additional_mass = fuel_for_mass(mass)
-    while fuel_for_mass(additional_mass) > 0:
-        additional_mass = fuel_for_mass(additional_mass)
-        module_fuel += additional_mass
-    total_fuel += module_fuel
+total_fuel = sum([iterative_fuel_for_mass(mass) for mass in modules])
 print("Real total fuel needed is: " + str(total_fuel))
