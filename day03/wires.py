3bfde52b4eb4b83e3065782eb819064ad276b30e
Mark Wanders <mark.wanders@ing.com>
2019-12-03 10:53:26 +0100

    Solved day 3

--- day03/wires.py
+++ day03/wires.py
@@ -1,4 +1,40 @@
 with open("input.txt") as my_file:
-    wires = my_file.readline()
-wires = [str(i) for i in wires.split(",")]
-print(wires)
+    wire1 = my_file.readline()
+    wire2 = my_file.readline()
+wire1 = [str(i) for i in wire1.split(",")]
+wire2 = [str(i) for i in wire2.split(",")]
+
+
+def calculate_lines(wire):
+    locations = [(0, 0, 0)]
+    for direction in wire:
+        steps = int(direction[1:])
+        current_location = locations[-1]
+        if direction.startswith("L"):
+            for x in range(current_location[0] - 1, current_location[0] - steps - 1, -1):
+                locations.append((x, current_location[1]))
+        elif direction.startswith("R"):
+            for x in range(current_location[0] + 1, current_location[0] + steps + 1):
+                locations.append((x, current_location[1]))
+        elif direction.startswith("U"):
+            for y in range(current_location[1] + 1, current_location[1] + steps + 1):
+                locations.append((current_location[0], y))
+        elif direction.startswith("D"):
+            for y in range(current_location[1] - 1, current_location[1] - steps - 1, -1):
+                locations.append((current_location[0], y))
+    return locations
+
+
+locations1 = calculate_lines(wire1)
+locations2 = calculate_lines(wire2)
+
+intersects = set(locations1[1:]).intersection(locations2[1:])
+
+manhattan_distances = [(abs(intersection[0]) + abs(intersection[1])) for intersection in intersects]
+
+# part one
+print(min(manhattan_distances))
+
+# part two
+step_distance = [(locations1.index(intersection) + locations2.index(intersection)) for intersection in intersects]
+print(min(step_distance))
