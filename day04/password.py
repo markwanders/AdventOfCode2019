3c35efe3468daac07239b91eaa12927193c56222
Mark Wanders <mark.wanders@ing.com>
2019-12-04 11:41:03 +0100

    Made day 4 a bit more readable

--- day04/password.py
+++ day04/password.py
@@ -1,11 +1,11 @@
 counter_part_one = 0
 counter_part_two = 0
-for p in range(130254, 678275):
-    d = any(c1 == c2 for c1, c2 in zip(str(p), str(p)[1:]))
-    lt = not any(c1 > c2 for c1, c2 in zip(str(p), str(p)[1:]))
-    if d and lt:
+for password in range(130254, 678275):
+    double = any(c1 == c2 for c1, c2 in zip(str(password), str(password)[1:]))
+    ascend = all(c1 <= c2 for c1, c2 in zip(str(password), str(password)[1:]))
+    if double and ascend:
         counter_part_one += 1
-        if any(str(p).count(c) == 2 for c in str(p)):
+        if any(str(password).count(c) == 2 for c in str(password)):
             counter_part_two += 1
 print(counter_part_one)
 print(counter_part_two)
