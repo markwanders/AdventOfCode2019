f59882616b2fc072c39152ba8f87c231324a9c49
Mark Wanders <mark.wanders@ing.com>
2019-12-04 09:32:30 +0100

    Better day 4

--- day04/password.py
+++ day04/password.py
@@ -3,10 +3,9 @@ counter_part_two = 0
 for p in range(130254, 678275):
     d = any(c1 == c2 for c1, c2 in zip(str(p), str(p)[1:]))
     lt = not any(c1 > c2 for c1, c2 in zip(str(p), str(p)[1:]))
-    dd = any(str(p).count(c) == 2 for c in str(p))
     if d and lt:
         counter_part_one += 1
-        if dd:
+        if any(str(p).count(c) == 2 for c in str(p)):
             counter_part_two += 1
 print(counter_part_one)
 print(counter_part_two)
