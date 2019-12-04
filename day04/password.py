dc3b9e746e6df9269bf466a46322a7cc5cea0fac
Mark Wanders <mark.wanders@ing.com>
2019-12-04 09:05:39 +0100

    Day 4 part two

--- day04/password.py
+++ day04/password.py
@@ -4,13 +4,13 @@ for p in range(130254, 678275):
     d = False
     lt = False
     for i in range(1, 6):
-        if ints[i] == ints[i - 1]:
-            d = True
         if ints[i] >= ints[i - 1]:
             lt = True
         else:
             lt = False
             break
+        if ints[i] == ints[i - 1] and str(p).count(str(ints[i])) == 2:
+            d = True
     if d and lt:
         counter = counter + 1
 print(counter)
