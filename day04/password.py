42a69551bc80b9458dad9f191aae927254f046af
Mark Wanders <mndmwanders@gmail.com>
2019-12-05 22:06:50 +0100

    Significantly sped up day 4

--- day04/password.py
+++ day04/password.py
@@ -1,11 +1,12 @@
 counter_part_one = 0
 counter_part_two = 0
 for password in range(130254, 678275):
-    double = any(c1 == c2 for c1, c2 in zip(str(password), str(password)[1:]))
     ascend = all(c1 <= c2 for c1, c2 in zip(str(password), str(password)[1:]))
-    if double and ascend:
-        counter_part_one += 1
-        if any(str(password).count(c) == 2 for c in str(password)):
-            counter_part_two += 1
+    if ascend:
+        double = any(c1 == c2 for c1, c2 in zip(str(password), str(password)[1:]))
+        if double:
+            counter_part_one += 1
+            if any(str(password).count(c) == 2 for c in str(password)):
+                counter_part_two += 1
 print(counter_part_one)
 print(counter_part_two)
