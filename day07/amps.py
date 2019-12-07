cc3e06e8b6e33237d067172a56f02093df014efb
Mark Wanders <mndmwanders@gmail.com>
2019-12-07 12:06:16 +0100

    Solved day 7 part two

--- day07/amps.py
+++ day07/amps.py
@@ -2,49 +2,74 @@ with open("input.txt") as f:
     ops = [int(i) for i in f.readline().split(",")]
 
 
-def run_program(phase, inp):
-    mem = ops[:]
-    index = 0
-    while index in range(len(mem)):
-        opcode = str(mem[index]).zfill(5)
-        if opcode.endswith("99"):
-            print("Quitting")
-            break
-        first = index + 1 if opcode[2] == "1" else mem[index + 1]
-        second = index + 2 if opcode[1] == "1" else mem[index + 2]
-        third = index + 3 if opcode[0] == "1" else mem[index + 3]
-        if opcode.endswith("1"):
-            mem[third] = mem[second] + mem[first]
-            index += 4
-        elif opcode.endswith("2"):
-            mem[third] = mem[second] * mem[first]
-            index += 4
-        elif opcode.endswith("3"):
-            mem[first] = inp if index > 0 else phase
-            index += 2
-        elif opcode.endswith("4"):
-            return mem[first]
-        elif opcode.endswith("5"):
-            index = mem[second] if mem[first] != 0 else index + 3
-        elif opcode.endswith("6"):
-            index = mem[second] if mem[first] == 0 else index + 3
-        elif opcode.endswith("7"):
-            mem[third] = 1 if mem[first] < mem[second] else 0
-            index += 4
-        elif opcode.endswith("8"):
-            mem[third] = 1 if mem[first] == mem[second] else 0
-            index += 4
+class Amplifier:
+    def __init__(self, code):
+        self.mem = code[:]
+        self.index = 0
+
+    def run_program(self, inputs):
+        while self.index in range(len(self.mem)):
+            opcode = str(self.mem[self.index]).zfill(5)
+            if opcode.endswith("99"):
+                break
+            first = self.index + 1 if opcode[2] == "1" else self.mem[self.index + 1]
+            second = self.index + 2 if opcode[1] == "1" else self.mem[self.index + 2]
+            if opcode.endswith(("1", "2", "7", "8")):
+                third = self.index + 3 if opcode[0] == "1" else self.mem[self.index + 3]
+            else:
+                third = 0
+            if opcode.endswith("1"):
+                self.mem[third] = self.mem[second] + self.mem[first]
+                self.index += 4
+            elif opcode.endswith("2"):
+                self.mem[third] = self.mem[second] * self.mem[first]
+                self.index += 4
+            elif opcode.endswith("3"):
+                self.mem[first] = inputs.pop(0)
+                self.index += 2
+            elif opcode.endswith("4"):
+                self.index += 2
+                return self.mem[first]
+            elif opcode.endswith("5"):
+                self.index = self.mem[second] if self.mem[first] != 0 else self.index + 3
+            elif opcode.endswith("6"):
+                self.index = self.mem[second] if self.mem[first] == 0 else self.index + 3
+            elif opcode.endswith("7"):
+                self.mem[third] = 1 if self.mem[first] < self.mem[second] else 0
+                self.index += 4
+            elif opcode.endswith("8"):
+                self.mem[third] = 1 if self.mem[first] == self.mem[second] else 0
+                self.index += 4
 
 
 # part one
-max_value = 0
 phases = {str(i).zfill(5) for i in range(0, 44444)}
 phases = {p for p in phases if (not any(p.count(c) > 1 for c in p)) and (not any(int(c) > 4 for c in p))}
-
+outputs = []
 for current_phase in phases:
     output = 0
-    for p in current_phase:
-        output = run_program(int(p), output)
-    if output > max_value:
-        max_value = output
-        print("Max value: %d for phase %s" % (max_value, current_phase))
+    amplifiers = [Amplifier(ops) for _ in range(5)]
+    for amplifier, phase in zip(amplifiers, current_phase):
+        output = amplifier.run_program(inputs=[int(phase), output])
+    outputs.append(output)
+print(max(outputs))
+
+# part two
+phases = {str(i).zfill(5) for i in range(55555, 99999)}
+phases = {p for p in phases if (not any(p.count(c) > 1 for c in p)) and (not any(int(c) < 5 for c in p))}
+for current_phase in phases:
+    amplifiers = [Amplifier(ops) for _ in range(5)]
+
+    output = 0
+    for amplifier, phase in zip(amplifiers, current_phase):
+        output = amplifier.run_program(inputs=[int(phase), output])
+
+    while True:
+        for amplifier in amplifiers:
+            output = amplifier.run_program(inputs=[output])
+
+        if output:
+            outputs.append(output)
+        else:
+            break
+print(max(outputs))
