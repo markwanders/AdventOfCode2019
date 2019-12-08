1f6fb029448be28440ab0d3d0e3ce2697ddf22cc
Mark Wanders <mndmwanders@gmail.com>
2019-12-08 09:36:09 +0100

    Solved day 8

---
+++ day08/image.py
@@ -0,0 +1,28 @@
+with open("input.txt") as f:
+    image = [char for char in str(f.readline())]
+
+x = 25
+y = 6
+
+layers = [image[i:i + (x * y)] for i in range(0, len(image), (x * y))]
+
+# part one
+zeroes = [layer.count("0") for layer in layers]
+min_zeroes = zeroes.index(min(zeroes))
+part_one = layers[min_zeroes].count("1") * layers[min_zeroes].count("2")
+print(part_one)
+
+# part two
+for _y in range(y):
+    line = ""
+    for _x in range(x):
+        pixel_nr = (_y * x) + _x
+        pixels = [layer[pixel_nr] for layer in layers]
+        for pixel in pixels:
+            if pixel == "0":
+                line += ".."
+                break
+            if pixel == "1":
+                line += "##"
+                break
+    print(line)
