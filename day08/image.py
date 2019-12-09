1b1e71f74a6332ce2df61c77a6bd0577e60a23cf
Mark Wanders <mark.wanders@ing.com>
2019-12-09 12:05:29 +0100

    Slightly more human readable output of day 8

--- day08/image.py
+++ day08/image.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 with open("input.txt") as f:
     image = [char for char in str(f.readline())]
 
@@ -6,9 +7,9 @@ x, y = 25, 6
 layers = [image[i:i + (x * y)] for i in range(0, len(image), (x * y))]
 
 # part one
-min_zeroes = min(layers, key=lambda layer: layer.count('0'))
+min_zeroes = min(layers, key=lambda l: l.count('0'))
 part_one = min_zeroes.count('1') * min_zeroes.count('2')
-print(part_one)
+print part_one
 
 # part two
 for _y in range(y):
@@ -18,9 +19,9 @@ for _y in range(y):
         pixels = [layer[pixel_nr] for layer in layers]
         for pixel in pixels:
             if pixel == "0":
-                line += ".."
+                line += "  "
                 break
             if pixel == "1":
-                line += "##"
+                line += "██"
                 break
-    print(line)
+    print line
