c1c81d7fc2b9065cf7d3946e57e87b57b6564245
Mark Wanders <mndmwanders@gmail.com>
2019-12-10 20:35:34 +0100

    Minor cleanup of day 10, still terrible

--- day10/asteroids.py
+++ day10/asteroids.py
@@ -11,25 +11,23 @@ def collinear(point1, point2, point3):
 
 
 def between(point1, point2, point3):
-    return (point2[0] <= point3[0] <= point1[0] or point1[0] <= point3[0] <= point2[0]) and (point2[1] <= point3[1] <= point1[1] or point1[1] <= point3[1] <= point2[1])
+    return (point2[0] <= point3[0] <= point1[0] or point1[0] <= point3[0] <= point2[0]) and \
+           (point2[1] <= point3[1] <= point1[1] or point1[1] <= point3[1] <= point2[1])
 
 
 def in_line_of_sight(point1, point2, point3):
     return collinear(point1, point2, point3) and between(point1, point2, point3)
 
 
-def angle(station, point):
-    dx = point[0] - station[0]
-    dy = point[1] - station[1]
+def angle(point1, point2):
+    dx = point2[0] - point1[0]
+    dy = point2[1] - point1[1]
 
-    angle = math.degrees(math.atan2(dx, -dy))
-    if angle < 0:
-        angle += 360
-    return angle
+    return math.degrees(math.atan2(dx, -dy)) % 360
 
 
-def distance(station, point):
-    return ((point[0] - station[0]) ** 2 + (point[1] - station[1]) ** 2) ** 0.5
+def distance(point1, point2):
+    return ((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2) ** 0.5
 
 
 asteroids = set()
@@ -41,7 +39,7 @@ for y in range(len(lines)):
 line_of_sight = {a: [] for a in asteroids}
 for asteroid1 in asteroids:
     for asteroid2 in (a for a in asteroids if a != asteroid1):
-        if not any([asteroid3 for asteroid3 in (a for a in asteroids if a != asteroid1 and a != asteroid2) if in_line_of_sight(asteroid1, asteroid2, asteroid3)]):
+        if not any([asteroid3 for asteroid3 in asteroids if asteroid3 != asteroid1 and asteroid3 != asteroid2 if in_line_of_sight(asteroid1, asteroid2, asteroid3)]):
             line_of_sight[asteroid1].append(asteroid2)
 station = max(line_of_sight, key=lambda v: len(line_of_sight[v]))
 print(station, len(line_of_sight[station]))
