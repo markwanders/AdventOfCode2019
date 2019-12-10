1c826127b5d5db0430a6b91c45e301e6f7a7d63b
Mark Wanders <mark.wanders@ing.com>
2019-12-10 13:32:55 +0100

    Day 10 prepare for part 2

--- day10/asteroids.py
+++ day10/asteroids.py
@@ -8,20 +8,8 @@ def collinear(point1, point2, point3):
            point3[0] * (point1[1] - point2[1]) == 0
 
 
-def manhattan(point1, point2):
-    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
-
-
 def between(point1, point2, point3):
-    if point1[0] > point2[0]:
-        between_x = point2[0] <= point3[0] <= point1[0]
-    else:
-        between_x = point1[0] <= point3[0] <= point2[0]
-    if point1[1] > point2[1]:
-        between_y = point2[1] <= point3[1] <= point1[1]
-    else:
-        between_y = point1[1] <= point3[1] <= point2[1]
-    return between_y and between_x
+    return (point2[0] <= point3[0] <= point1[0] or point1[0] <= point3[0] <= point2[0]) and (point2[1] <= point3[1] <= point1[1] or point1[1] <= point3[1] <= point2[1])
 
 
 def in_line_of_sight(point1, point2, point3):
@@ -34,10 +22,10 @@ for y in range(len(lines)):
         if lines[y][x] == '#':
             asteroids.add((x, y))
 
-line_of_sight = {a: 0 for a in asteroids}
+line_of_sight = {a: [] for a in asteroids}
 for asteroid1 in asteroids:
     for asteroid2 in (a for a in asteroids if a != asteroid1):
         if not any([asteroid3 for asteroid3 in (a for a in asteroids if a != asteroid1 and a != asteroid2) if in_line_of_sight(asteroid1, asteroid2, asteroid3)]):
-            line_of_sight[asteroid1] += 1
-station = max(line_of_sight, key=line_of_sight.get)
-print(line_of_sight[station])
+            line_of_sight[asteroid1].append(asteroid2)
+station = max(line_of_sight, key=lambda v: len(line_of_sight[v]))
+print(len(line_of_sight[station]))
