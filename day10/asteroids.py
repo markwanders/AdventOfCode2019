a115c4f7b61e24013688042c8dad20f39c8b43a3
Mark Wanders <mark.wanders@ing.com>
2019-12-10 14:49:59 +0100

    Solved day 10 part 2

--- day10/asteroids.py
+++ day10/asteroids.py
@@ -1,3 +1,5 @@
+import math
+
 with open("input.txt") as f:
     lines = f.read().split()
 
@@ -16,6 +18,20 @@ def in_line_of_sight(point1, point2, point3):
     return collinear(point1, point2, point3) and between(point1, point2, point3)
 
 
+def angle(station, point):
+    dx = point[0] - station[0]
+    dy = point[1] - station[1]
+
+    angle = math.degrees(math.atan2(dx, -dy))
+    if angle < 0:
+        angle += 360
+    return angle
+
+
+def distance(station, point):
+    return ((point[0] - station[0]) ** 2 + (point[1] - station[1]) ** 2) ** 0.5
+
+
 asteroids = set()
 for y in range(len(lines)):
     for x in range(len(lines[y])):
@@ -28,4 +44,7 @@ for asteroid1 in asteroids:
         if not any([asteroid3 for asteroid3 in (a for a in asteroids if a != asteroid1 and a != asteroid2) if in_line_of_sight(asteroid1, asteroid2, asteroid3)]):
             line_of_sight[asteroid1].append(asteroid2)
 station = max(line_of_sight, key=lambda v: len(line_of_sight[v]))
-print(len(line_of_sight[station]))
+print(station, len(line_of_sight[station]))
+
+to_destroy = sorted(line_of_sight[station], key=lambda a: (angle(station, a), -distance(station, a)))
+print(100 * to_destroy[199][0] + to_destroy[199][1])
