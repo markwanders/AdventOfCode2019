b8fd02f5ed92a884b2956c3d7aad92f735db9109
Mark Wanders <mark.wanders@ing.com>
2019-12-12 15:30:33 +0100

    Day12 more pythonic

--- day12/moons.py
+++ day12/moons.py
@@ -6,8 +6,8 @@ with open("input.txt") as f:
     moons = [map(int, re.findall(r'-?\d+', line)) for line in f.readlines()]
 
 positions = {i: moons[i] for i in range(len(moons))}
-velocities = {i: [0, 0, 0] for i in range(len(moons))}
-repeats = [0, 0, 0]
+velocities = {i: [0] * len(positions[0]) for i in range(len(moons))}
+repeats = [0] * len(positions[0])
 
 step = 1
 proceed = True
@@ -15,16 +15,16 @@ while any(i == 0 for i in repeats):
     for pair in itertools.combinations(positions.keys(), 2):
         position1, position2 = positions[pair[0]], positions[pair[1]]
         velocities1, velocities2 = velocities[pair[0]], velocities[pair[1]]
-        for d in range(3):
-            if position1[d] > position2[d]:
+        for d, position in enumerate(position1):
+            if position > position2[d]:
                 velocities1[d] -= 1
                 velocities2[d] += 1
-            elif position1[d] < position2[d]:
+            elif position < position2[d]:
                 velocities1[d] += 1
                 velocities2[d] -= 1
     positions = {moon: [sum(pv) for pv in zip(positions[moon], velocities[moon])] for moon in positions.keys()}
-    for d in range(3):
-        if repeats[d] == 0 and all((positions[m][d], velocities[m][d]) == (moons[m][d], 0) for m in positions.keys()):
+    for d, repeat in enumerate(repeats):
+        if repeat == 0 and all((positions[m][d], velocities[m][d]) == (moons[m][d], 0) for m in positions.keys()):
             repeats[d] = step
     # part one
     if step == 1000:
