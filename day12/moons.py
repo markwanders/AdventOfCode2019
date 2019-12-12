62a38a1e7f68e1a0485df8cb873055e18fe86a38
Mark Wanders <mark.wanders@ing.com>
2019-12-12 12:11:20 +0100

    Minor cleanup day12

--- day12/moons.py
+++ day12/moons.py
@@ -14,18 +14,18 @@ proceed = True
 while any(i == 0 for i in repeats):
     for pair in itertools.combinations(positions.keys(), 2):
         position1, position2 = positions[pair[0]], positions[pair[1]]
-        for c in range(3):
-            if position1[c] > position2[c]:
-                velocities[pair[0]][c] -= 1
-                velocities[pair[1]][c] += 1
-            elif position1[c] < position2[c]:
-                velocities[pair[0]][c] += 1
-                velocities[pair[1]][c] -= 1
-    for moon in positions.keys():
-        positions[moon] = [sum(x) for x in zip(positions[moon], velocities[moon])]
-    for c in range(3):
-        if repeats[c] == 0 and all((positions[m][c], velocities[m][c]) == (moons[m][c], 0) for m in positions.keys()):
-            repeats[c] = step
+        velocities1, velocities2 = velocities[pair[0]], velocities[pair[1]]
+        for d in range(3):
+            if position1[d] > position2[d]:
+                velocities1[d] -= 1
+                velocities2[d] += 1
+            elif position1[d] < position2[d]:
+                velocities1[d] += 1
+                velocities2[d] -= 1
+    positions = {moon: [sum(pv) for pv in zip(positions[moon], velocities[moon])] for moon in positions.keys()}
+    for d in range(3):
+        if repeats[d] == 0 and all((positions[m][d], velocities[m][d]) == (moons[m][d], 0) for m in positions.keys()):
+            repeats[d] = step
     # part one
     if step == 1000:
         print(sum([sum(map(abs, positions[moon])) * sum(map(abs, velocities[moon])) for moon in positions.keys()]))
