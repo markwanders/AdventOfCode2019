0bf64a5b906a87663eafe4f77511514266152b46
Mark Wanders <mndmwanders@gmail.com>
2019-12-15 14:09:09 +0100

    Solved day 14 part two

--- day14/chemicals.py
+++ day14/chemicals.py
@@ -1,24 +1,36 @@
 import re
 
+from math import floor
+
 with open("input.txt") as f:
     reactions = [[(re.findall('[A-Z]+', r)[0], int(re.findall(r'\d+', r)[0])) for r in re.findall(r'\d+ [A-Z]+', line)] for line in f.readlines()]
 reaction_map = {r[-1]: r[:-1] for r in reactions}
-inventory = {r[0]: 0 for r in reaction_map.keys()}
-inventory["ORE"] = 0
 
 
-def react(output, inputs):
-    for c in inputs:
-        if c[0] == "ORE":
-            inventory["ORE"] += c[1]
-        else:
-            while inventory[c[0]] < c[1]:
-                new_output = [k for k in reaction_map.keys() if c[0] in k][0]
-                new_input = reaction_map[new_output]
-                react(new_output, new_input)
-            inventory[c[0]] -= c[1]
-    inventory[output[0]] += output[1]
+def ore_for_fuel(amount):
+    required_chemical_amounts = {'FUEL': amount}
+
+    while [k for (k, v) in required_chemical_amounts.items() if k != "ORE" and v > 0]:
+        for chemical in [k for (k, v) in required_chemical_amounts.items() if k != "ORE" and v > 0]:
+            output = [k for k in reaction_map.keys() if k[0] == chemical][0]
+            quantity = output[1]
+            reaction = reaction_map[output]
+            multiple = (required_chemical_amounts[chemical] + quantity - 1) // quantity
+            for (input_element, input_quantity) in reaction:
+                required_chemical_amounts[input_element] = required_chemical_amounts.get(input_element, 0) + multiple * input_quantity
+            required_chemical_amounts[chemical] -= multiple * quantity
+
+    return required_chemical_amounts["ORE"]
 
 
-react(('FUEL', 1), reaction_map[('FUEL', 1)])
-print(inventory["ORE"])
+print(ore_for_fuel(1))
+
+fuel = 1
+target = 1e12
+while True:
+    ore = ore_for_fuel(fuel + 1)
+    if ore > target:
+        print(fuel)
+        break
+    else:
+        fuel = max(fuel + 1, floor((fuel + 1) * target / ore))
