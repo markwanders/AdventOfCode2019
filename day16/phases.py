47ade619c51fb6b42dec0547426620615c197a60
Mark Wanders <mark.wanders@ing.com>
2019-12-16 10:28:07 +0100

    Solved day 16 part one

--- day16/phases.py
+++ day16/phases.py
@@ -1,14 +1,15 @@
-from itertools import cycle
+from itertools import cycle, islice
+
 with open("input.txt") as f:
     sequence = [int(i) for i in f.readline()]
 
 base_pattern = [0, 1, 0, -1]
 
-output = []
-input_number = sequence[0]
-for index in range(len(sequence)):
-    pattern = [val for val in base_pattern for _ in range(index + 1)]
-    pool = cycle(pattern)
-    pattern_number = next(pool)
-    print(sequence[index], pattern_number)
-print(output)
+for p in range(0, 100):
+    output = []
+    for index in range(len(sequence)):
+        pattern = list(islice(cycle([val for val in base_pattern for _ in range(index + 1)]), len(sequence) + 1))[1:]
+        number = abs(sum([a * b for a, b in zip(sequence, pattern)])) % 10
+        output.append(number)
+    sequence = output
+print ''.join(map(str, sequence[:8]))
