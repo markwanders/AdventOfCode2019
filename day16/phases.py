e4ee27887b4890f56ba9d445fc0dd9c929912aa2
Mark Wanders <mndmwanders@gmail.com>
2019-12-16 20:15:24 +0100

    Solved day 16 part two

--- day16/phases.py
+++ day16/phases.py
@@ -7,7 +7,7 @@ base_pattern = [0, 1, 0, -1]
 
 
 def fft(input_sequence):
-    for p in range(0, 100):
+    for _ in range(100):
         output = []
         for index in range(len(input_sequence)):
             pattern = list(islice(cycle([val for val in base_pattern for _ in range(index + 1)]), len(input_sequence) + 1))[1:]
@@ -18,8 +18,16 @@ def fft(input_sequence):
 
 
 # part one
-print ''.join(map(str, fft(sequence)[:8]))
+print(''.join(map(str, fft(sequence)[:8])))
 
 # part two
+# using the fact that for any sequence of length n, we know that for the final number, the phase is 0,...,0,1,
+# and so the phase shifted final number s'[n] = 1 * s[n], and for the second to last number the phase is 0,...0,1,1
+# (this is because our offset places us far beyond the halfway point of the sequence, and so we don't encounter -1!)
+# and thus s'[n-1] = (s[n-1] + s'[n]) % 10 = (s[n-1] + s[n]) % 10, we can just count backwards from the end
 offset = num = int(''.join(map(str, sequence[:7])))
-print ''.join(map(str, fft(sequence * 10000)[offset:8 + offset]))
+sequence = (sequence * 10000)[offset:]
+for _ in range(100):
+    for n in range(-2, -len(sequence) - 1, -1):
+        sequence[n] = (sequence[n] + sequence[n + 1]) % 10
+print(''.join(map(str, sequence[:8])))
