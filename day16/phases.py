6d6e7587b2103e671bedd7e387454d9ee7c71263
Mark Wanders <mark.wanders@ing.com>
2019-12-16 11:03:07 +0100

    Day 16 part two but only if you wait a couple of years

--- day16/phases.py
+++ day16/phases.py
@@ -5,11 +5,21 @@ with open("input.txt") as f:
 
 base_pattern = [0, 1, 0, -1]
 
-for p in range(0, 100):
-    output = []
-    for index in range(len(sequence)):
-        pattern = list(islice(cycle([val for val in base_pattern for _ in range(index + 1)]), len(sequence) + 1))[1:]
-        number = abs(sum([a * b for a, b in zip(sequence, pattern)])) % 10
-        output.append(number)
-    sequence = output
-print ''.join(map(str, sequence[:8]))
+
+def fft(input_sequence):
+    for p in range(0, 100):
+        output = []
+        for index in range(len(input_sequence)):
+            pattern = list(islice(cycle([val for val in base_pattern for _ in range(index + 1)]), len(input_sequence) + 1))[1:]
+            number = abs(sum([a * b for a, b in zip(input_sequence, pattern)])) % 10
+            output.append(number)
+        input_sequence = output
+    return input_sequence
+
+
+# part one
+print ''.join(map(str, fft(sequence)[:8]))
+
+# part two
+offset = num = int(''.join(map(str, sequence[:7])))
+print ''.join(map(str, fft(sequence * 10000)[offset:8 + offset]))
