111ac6a65135e2eff071970cc2ff6b44c222f636
Mark Wanders <mark.wanders@ing.com>
2019-12-18 16:25:44 +0100

    Solved day 18 part one

--- day18/keys.py
+++ day18/keys.py
@@ -1,4 +1,4 @@
-import heapq
+from queue import Queue
 
 with open("input.txt") as f:
     raw_cave = [line.rstrip() for line in f]
@@ -12,26 +12,30 @@ for _y in raw_cave:
         x += 1
     y += 1
 
-keys = {(k, v) for (k, v) in cave.items() if ord(v) in range(97, 123)}
-collected_keys = []
-print(keys)
+robot = [k for (k, v) in cave.items() if v == '@'][0]
+keys = {v for (k, v) in cave.items() if ord(v) in range(97, 123)}
 
-def dijkstra(grid, origin, target):
-    queue = []
-    visited = {}
-    heapq.heappush(queue, (origin, 0))
+q = Queue()
+q.put((robot, [], 0))
+visited = {(robot, tuple())}
 
-    while queue:
+while True:
+    robot, found_keys, steps = q.get()
+    found_keys = set(found_keys)
+    if found_keys == keys:
+        print("All keys found!")
+        break
+    neighbors = [(robot[0] - 1, robot[1]), (robot[0] + 1, robot[1]), (robot[0], robot[1] - 1), (robot[0], robot[1] + 1)]
+    for neighbor in neighbors:
+        tile = str(cave[neighbor])
+        if tile != '#':
+            if not ('A' <= tile <= 'Z' and tile.lower() not in found_keys):  # i.e. either an open spot or an open door
+                found_keys_for_node = found_keys
+                if 'a' <= tile <= 'z' and tile not in found_keys_for_node:  # i.e. this is a new key
+                    found_keys_for_node = found_keys_for_node | {tile}
+                node = (neighbor, tuple(sorted(found_keys_for_node)))
+                if node not in visited:
+                    visited.add(node)
+                    q.put((neighbor, node[1], steps + 1))
 
-        current_node = heapq.heappop(queue)
-        # if already visited with an equal or shorter distance, skip
-        if current_node in visited.keys() and visited[current_node] <= current_node[1]:
-            continue
-
-        visited[current_node] = 1
-
-        if current_node[0] == target:
-            return current_node[1]
-
-        square = grid[current_node[0]]
-        if
+print(steps)
