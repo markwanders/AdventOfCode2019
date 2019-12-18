508d74d4c7a700bfbc5bdcc85c8aebc37b9dc8aa
Mark Wanders <mark.wanders@ing.com>
2019-12-18 17:34:20 +0100

    Solved day 18 part two except for a weird off-by-8 error

--- day18/keys.py
+++ day18/keys.py
@@ -12,12 +12,13 @@ for _y in raw_cave:
         x += 1
     y += 1
 
-robot = [k for (k, v) in cave.items() if v == '@'][0]
+origin = [k for (k, v) in cave.items() if v == '@'][0]
 keys = {v for (k, v) in cave.items() if ord(v) in range(97, 123)}
 
+# part one
 q = Queue()
-q.put((robot, [], 0))
-visited = {(robot, tuple())}
+q.put((origin, [], 0))
+visited = {(origin, tuple())}
 
 while True:
     robot, found_keys, steps = q.get()
@@ -37,5 +38,39 @@ while True:
                 if node not in visited:
                     visited.add(node)
                     q.put((neighbor, node[1], steps + 1))
+
+print(steps)
+# part two
+for key in [origin, (origin[0] + 1, origin[1]), (origin[0] - 1, origin[1]), (origin[0], origin[1] + 1), (origin[0], origin[1] - 1)]:
+    cave[key] = "#"
+
+origins = [(origin[0] - 1, origin[1] - 1), (origin[0] + 1, origin[1] - 1), (origin[0] + 1, origin[1] + 1), (origin[0] - 1, origin[1] + 1)]
+q = Queue()
+q.put(((origins[0], origins[1], origins[2], origins[3]), tuple(), 0))
+visited = [{(origins[0], tuple())}, {(origins[1], tuple())}, {(origins[2], tuple())}, {(origins[3], tuple())}]
+
+while True:
+    robots, found_keys, steps = q.get()
+    robots = list(robots)
+    found_keys = set(found_keys)
+    if found_keys == keys:
+        print("All keys found!")
+        break
+
+    for r in range(len(robots)):
+        robot = robots[r]
+        neighbors = [(robot[0] - 1, robot[1]), (robot[0] + 1, robot[1]), (robot[0], robot[1] - 1), (robot[0], robot[1] + 1)]
+        for neighbor in neighbors:
+            tile = str(cave[neighbor])
+            if tile != '#':
+                if not ('A' <= tile <= 'Z' and tile.lower() not in found_keys):
+                    found_keys_for_node = found_keys
+                    robots[r] = neighbor
+                    if 'a' <= tile <= 'z' and tile not in found_keys_for_node:
+                        found_keys_for_node = found_keys_for_node | {tile}
+                    node = (neighbor, tuple(sorted(found_keys_for_node)))
+                    if node not in visited[r]:
+                        visited[r].add(node)
+                        q.put((tuple(robots), node[1], steps + 1))
 
 print(steps)
