2cc634909f6e1b15ce07e4920299643b2f521816
Mark Wanders <mndmwanders@gmail.com>
2019-12-20 10:44:46 +0100

    Solved day 20 part two

--- day20/torus.py
+++ day20/torus.py
@@ -5,7 +5,7 @@ with open("input.txt") as f:
 
 torus = {}
 warps = {}
-y = 0
+max_y = max_x = y = 0
 for _y in raw_torus:
     x = 0
     for _x in _y:
@@ -20,12 +20,16 @@ for _y in raw_torus:
                 warps[warp_point] = warp
             if (x, y - 1) in torus.keys() and 'A' <= torus[(x, y - 1)] <= 'Z':
                 warp = torus[(x, y - 1)] + _x
-                if(x, y - 2) in torus.keys() and torus[(x, y - 2)] == '.':
+                if (x, y - 2) in torus.keys() and torus[(x, y - 2)] == '.':
                     warp_point = (x, y - 2)
                 else:
                     warp_point = (x, y + 1)
                 warps[warp_point] = warp
+        if x > max_x:
+            max_x = x
         x += 1
+    if y > max_y:
+        max_y = y
     y += 1
 
 warp_to_warp = {k: k2 for (k, v) in warps.items() for (k2, v2) in warps.items() if v == v2 and k2 != k}
@@ -50,6 +54,37 @@ while True:
             if neighbor not in visited:
                 visited.append(neighbor)
                 q.put((neighbor, steps + 1))
+print(steps)
+
+# part two
+q = Queue()
+q.put((origin, 0, 0))
+distances = {(origin, 0): 0}
 
 
+def is_outer(a):
+    if a[0] == 2 or a[1] == 2:
+        return True
+    if a[0] == max_x - 2 or a[1] == max_y - 2:
+        return True
+    return False
+
+
+while True:
+    p, level, steps = q.get()
+    if p == target and level == 0:
+        break
+    neighbors = [(p[0] - 1, p[1]), (p[0] + 1, p[1]), (p[0], p[1] - 1), (p[0], p[1] + 1)]
+    for neighbor in neighbors:
+        tile = torus.get(neighbor, '#')
+        if tile == '.':
+            if neighbor in warp_to_warp.keys() and (warp_to_warp[neighbor], level) not in visited:
+                if is_outer(neighbor):
+                    if level != 0:
+                        q.put((warp_to_warp[neighbor], level - 1, steps + 2))
+                else:
+                    q.put((warp_to_warp[neighbor], level + 1, steps + 2))
+            if (neighbor, level) not in distances.keys() or distances[(neighbor, level)] > steps + 1:
+                distances[(neighbor, level)] = steps + 1
+                q.put((neighbor, level, steps + 1))
 print(steps)
