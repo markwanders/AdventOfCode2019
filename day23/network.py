798524dc99e7f29807ceeedcd7c37250a2b2798f
Mark Wanders <mark.wanders@ing.com>
2019-12-23 10:37:37 +0100

    Solved day 23 part one

--- day23/network.py
+++ day23/network.py
@@ -1,22 +1,17 @@
 with open("input.txt") as f:
     ops = [int(i) for i in f.readline().split(",")]
 
-
-def print_grid(grid):
-    line = ""
-    for _y in range(max(grid.keys())[1] + 1):
-        for _x in range(max(grid.keys())[0] + 1):
-            line += str(chr(grid.get((_x, _y), 46)))
-        line += "\n"
-    print(line)
+packets = {a: [a] for a in range(50)}
+packets[255] = []
 
 
 class Computer:
-    def __init__(self, code):
+    def __init__(self, code, address):
         self.mem = (code[:] + [0] * 10000)[:10000]
         self.index = 0
         self.relative = 0
-        self.inputs = []
+        self.address = address
+        self.output = []
 
     def parameter_mode(self, op, param):
         if op[3 - param] == "0":
@@ -41,11 +36,19 @@ class Computer:
                 self.mem[third] = self.mem[second] * self.mem[first]
                 self.index += 4
             elif opcode.endswith("3"):
-                self.mem[first] = self.inputs.pop(0)
+                if not packets[self.address]:
+                    self.mem[first] = -1
+                else:
+                    self.mem[first] = packets[self.address].pop(0)
                 self.index += 2
+                break
             elif opcode.endswith("4"):
                 self.index += 2
-                return self.mem[first]
+                self.output += [self.mem[first]]
+                if len(self.output) == 3:
+                    packets[self.output[0]] += self.output[1:3]
+                    self.output = self.output[3:]
+                    break
             elif opcode.endswith("5"):
                 self.index = self.mem[second] if self.mem[first] != 0 else self.index + 3
             elif opcode.endswith("6"):
@@ -61,47 +64,8 @@ class Computer:
                 self.index += 2
 
 
-def adjacent(xx, yy):
-    return [(xx + 1, yy), (xx - 1, yy), (xx, yy + 1), (xx, yy - 1)]
-
-
-robot = Computer(ops)
-outputs = {}
-x = y = 0
-while True:
-    output = robot.run_program()
-    if output:
-        if output == 10:
-            y += 1
-            x = 0
-        else:
-            outputs[(x, y)] = output
-            x += 1
-    else:
-        break
-
-print_grid(outputs)
-# part one
-scaffolds = [k for (k, v) in outputs.items() if v == 35]
-intersections = [s for s in scaffolds if all(elem in scaffolds for elem in adjacent(s[0], s[1]))]
-print(sum(p[0] * abs(p[1]) for p in intersections))
-
-# part two
-# worked out manually:
-# the path is L10R8R8L10R8R8L10L12R8R10R10L12R10L10L12R8R10R10L12R10L10L12R8R10R10L12R10R10L12R10L10R8R8
-# meaning A = L10R8R8, B = L10L12R8R10, C = R10L12R10 and the path is AABCBCBCCA
-
-move_ops = ops[:]
-move_ops[0] = 2
-move_robot = Computer(move_ops)
-move_robot.inputs = list(map(ord, "A,A,B,C,B,C,B,C,C,A\nL,10,R,8,R,8\nL,10,L,12,R,8,R,10\nR,10,L,12,R,10\nn\n"))
-part_two_output = []
-
-while True:
-    output = move_robot.run_program()
-    if output:
-        part_two_output.append(output)
-    else:
-        break
-
-print(part_two_output[-1])
+computers = [Computer(ops, a) for a in range(50)]
+while not packets[255]:
+    for computer in computers:
+        computer.run_program()
+print(packets[255])
